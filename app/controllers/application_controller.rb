55910385edffab0158bb04e4a44857e5ef89ac6c
sumitsv21 <koolsumit21@gmail.com>
2015-04-25 16:59:49 +0530

    authentications twitter done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,38 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+
+  helper_method :resource, :resource_name, :devise_mapping
+
+  private
+
+  def after_sign_out_path_for(resource)
+    if resource.eql?(:user)
+      root_path
+    end 
+  end
+
+  def after_sign_in_path_for(resource)
+    if resource.class.eql?(User)
+       session[:previous_url] || root_path
+    end
+  end
+
+  def after_sign_up_path_for(resource)
+    if resource.eql?(:user)
+       root_path
+    end
+  end
+ 
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150425090541) do
+ActiveRecord::Schema.define(version: 20150425104633) do
+
+  create_table "authentications", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.string   "provider",     limit: 255
+    t.string   "uid",          limit: 255
+    t.string   "image",        limit: 255
+    t.text     "token",        limit: 65535
+    t.text     "token_secret", limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
