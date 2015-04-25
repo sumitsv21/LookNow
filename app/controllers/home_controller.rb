844753cc00d83764431039b198089aaac8337f5a
Nitesh Kumar <nitesh9952@gmail.com>
2015-04-25 21:47:06 +0530

    Added live tweet from app

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
 
 
 	def index
-		
+		@property_infos = PropertyInfo.order("created_at DESC").first(10)
 	end
 	
 end
--- app/controllers/property_infos_controller.rb
+++ app/controllers/property_infos_controller.rb
@@ -1,5 +1,13 @@
 class PropertyInfosController < ApplicationController
   def create
-
+    if params[:name] == "price"
+      details = params["price_value"]
+    elsif params[:name] == "infra"
+      details = params["infra_value"]
+    end
+    @property_info = PropertyInfo.create(property_id: 1, name: params[:name], comments: params[:comments], details: details.to_json)
+    respond_to do |format|
+      format.js
+    end
   end
 end
