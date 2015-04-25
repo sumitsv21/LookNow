81582c97c9dd6bf1477d1f7bc5dd1aa6ec810373
thepraveen0207 <praveen.sah@flipkart.com>
2015-04-25 17:00:32 +0530

    addedd more classes and act as taggale

--- db/migrate/20150425101609_create_properties.rb
+++ db/migrate/20150425101609_create_properties.rb
@@ -1,13 +1,13 @@
 class CreateProperties < ActiveRecord::Migration
   def change
     create_table :properties do |t|
-      # t.string :status
-      # t.column :user_id
+      t.string :status
+      t.column :user_id, :bigint, :null=>false
       t.float :latitude
       t.float :longitude
       t.text :boundary
-      # t.string :type
-      # t.string :sub_type
+      t.string :typee
+      t.string :sub_type
       t.string :landmark
       t.timestamps null: false
     end
