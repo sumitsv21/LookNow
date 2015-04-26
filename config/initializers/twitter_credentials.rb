6349855e4ab4538aacd6c8600c3cd8e2527e367e
sumitsv21 <koolsumit21@gmail.com>
2015-04-26 07:15:20 +0530

    new app

--- config/initializers/twitter_credentials.rb
+++ config/initializers/twitter_credentials.rb
@@ -1,15 +1,24 @@
 require 'twitter'
 
+# TWITTER_CLIENT = Twitter::REST::Client.new do |config|
+#                   config.consumer_key = 'slmLB86di1vEn5zl2bbMt4BiG'
+#                   config.consumer_secret = 'hdKSRVrcguXVkaUbnKC0nlh0QKvaTv21RUdcgsC5d6quvwYBOo'
+#                   config.oauth_token = '94298024-6HVxrjPLacZe7rxdr1rYEeXgvJJZ1rollMx4b5x5y'
+#                   config.oauth_token_secret = 'TKLf95PEQaHG3USiuP3fP1noQhmQUAI0Vp8XHjw0fUcX3'
+#                 end
+
+# TWITTER_STREAM_CLIENT = Twitter::Streaming::Client.new do |config|
+#                             config.consumer_key = 'slmLB86di1vEn5zl2bbMt4BiG'
+#                             config.consumer_secret = 'hdKSRVrcguXVkaUbnKC0nlh0QKvaTv21RUdcgsC5d6quvwYBOo'
+#                             config.oauth_token = '94298024-6HVxrjPLacZe7rxdr1rYEeXgvJJZ1rollMx4b5x5y'
+#                             config.oauth_token_secret = 'TKLf95PEQaHG3USiuP3fP1noQhmQUAI0Vp8XHjw0fUcX3'
+#                         end
+
 TWITTER_CLIENT = Twitter::REST::Client.new do |config|
-                  config.consumer_key = 'slmLB86di1vEn5zl2bbMt4BiG'
-                  config.consumer_secret = 'hdKSRVrcguXVkaUbnKC0nlh0QKvaTv21RUdcgsC5d6quvwYBOo'
-                  config.oauth_token = '94298024-6HVxrjPLacZe7rxdr1rYEeXgvJJZ1rollMx4b5x5y'
-                  config.oauth_token_secret = 'TKLf95PEQaHG3USiuP3fP1noQhmQUAI0Vp8XHjw0fUcX3'
+                  config.consumer_key = 'EgnavLeK4rkzrsQBGecraUPI1'
+                  config.consumer_secret = 'NTHYMqZHCQsgo8FxBChK7B8RwpcwKKAJsQztnEILHcKgbNIFf2'
+                  config.oauth_token = '94298024-5Iafkcb17TuPNnZlrnTxX4HhpQkX0XX5CVHZ6g2Xg'
+                  config.oauth_token_secret = '7jgUiljKk4zPCrRxVWHg5amcgOVVYqRCTKessBU0PG61f'
                 end
 
-TWITTER_STREAM_CLIENT = Twitter::Streaming::Client.new do |config|
-                            config.consumer_key = 'slmLB86di1vEn5zl2bbMt4BiG'
-                            config.consumer_secret = 'hdKSRVrcguXVkaUbnKC0nlh0QKvaTv21RUdcgsC5d6quvwYBOo'
-                            config.oauth_token = '94298024-6HVxrjPLacZe7rxdr1rYEeXgvJJZ1rollMx4b5x5y'
-                            config.oauth_token_secret = 'TKLf95PEQaHG3USiuP3fP1noQhmQUAI0Vp8XHjw0fUcX3'
-                        end
+                
