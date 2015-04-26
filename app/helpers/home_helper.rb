8d0334c13696a54499873a3808307c693eedc58d
thepraveen0207 <thepraveen0207@gmail.com>
2015-04-26 06:29:06 +0530

    changd get live tweet

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -7,11 +7,7 @@ module HomeHelper
     TWITTER_CLIENT.update(str)
   end
 
-  def get_live_tweets(topics = ['housing', 'land', 'plot', 'bangalore'])
-    a = []
-    TWITTER_STREAM_CLIENT.filter(:track => topics.join(",")) do |object|
-      a << object if object.is_a?(Twitter::Tweet)
-      return a if a.size > 10
-    end
+  def get_live_tweets(search_params = ['housing', 'land', 'plot', 'bangalore'], opts = {result_type: "recent"})
+    TWITTER_CLIENT.search(search_params.join(" or "), opts).map{ |tweet| {:user => tweet.user, :count => tweet.user.followers_count, :favorite_count => tweet.favorite_count, :retweet_count => tweet.retweet_count, :id => tweet.id, :text => tweet.text}}
   end
 end
